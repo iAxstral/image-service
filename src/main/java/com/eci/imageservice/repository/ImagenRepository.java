201b4a79b4129e38f5d6e0b43e5d62c67ea5e7ff
Tomas <tomasdi.dev@gmail.com>
2026-03-27 23:06:16 -0500

    feat 19: add mongo image repository

--- src/main/java/com/eci/imageservice/repository/ImagenRepository.java
+++ src/main/java/com/eci/imageservice/repository/ImagenRepository.java
@@ -1,10 +1,10 @@
 package com.eci.imageservice.repository;
-
-import com.eci.imageservice.model.document.ImagenDocument;
+import com.example.imageservice.model.document.ImagenDocument;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 import java.util.List;
 
 public interface ImagenRepository extends MongoRepository<ImagenDocument, String> {
+
     List<ImagenDocument> findByReferenciaExterna(String referenciaExterna);
 }
